fb39662cf09fd4d3fb1879819afc8b7dc570581c
krkmtk <krosovok2019@gmail.com>
2025-05-10 00:35:38 +0300

    Edited gitignore and removed unused function

--- Sneekstore/botstore/handlers/basic.py
+++ Sneekstore/botstore/handlers/basic.py
@@ -6,19 +6,5 @@ router = Router()
 @router.message(CommandStart())
 async def cmd_start(message: types.Message) -> None:
     await message.answer(
-        "👋 Привет! Я готов к работе.\n"
-        "Список команд: /help"
+        "👋 Вітаю в магазині Sneekshop\n"
     )
-
-@router.message(Command(commands={"help"}))
-async def cmd_help(message: types.Message) -> None:
-    await message.answer(
-        "/start — приветствие\n"
-        "/help — справка\n"
-        "Напишите что‑нибудь, и я повторю это."
-    )
-
-@router.message()
-async def echo_all(message: types.Message) -> None:
-    if message.text:
-        await message.answer(message.text)
